656b382a1e2b8dcb80e0c63f713419d17dc466ad
arminboom <quingleguy@gmail.com>
2024-11-14 21:24:10 +0100

    aaaa
    jjj

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function Onclick() {
     if (click === 1)
     alert("Ah, i see, you found one of the secrets in this website, however you will need a code for this. Think OUTSIDE of the box...")
     if (click === 10)
-    window.open("http://127.0.0.1:5500/armin.html")
+    window.open("https://arminboom.github.io/me/armin.html")
 }
 
