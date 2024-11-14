fba37b21cfdecd37e6ab2f9d92b25a5be540011b
arminboom <quingleguy@gmail.com>
2024-11-14 20:57:50 +0100

    Hello
    :)

---
+++ index.js
@@ -0,0 +1,10 @@
+let click = 0
+
+function Onclick() {
+    click++
+    if (click === 1)
+    alert("Ah, i see, you found one of the secrets in this website, however you will need a code for this. Think OUTSIDE of the box...")
+    if (click === 10)
+    window.open("http://127.0.0.1:5500/armin.html")
+}
+
